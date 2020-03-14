55abf2224e531103d2b176577785a0a5ab56bf4f
p1q <admin2k7@gmail.com>
2020-03-14 15:00:10 +0200

    Step 1. Implement the simple Spring Boot

---
+++ src/main/java/com/reviews/amazon/amazonreviews/AmazonReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.reviews.amazon.amazonreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewsApplication.class, args);
+    }
+
+}
