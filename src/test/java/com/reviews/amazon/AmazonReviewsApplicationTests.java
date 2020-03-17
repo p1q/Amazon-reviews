1b616864cd84a28479de2d90d519572d4fcb9a67
AdminPC\Admin <admin2k7@gmail.com>
2020-03-17 02:01:57 +0200

    Step 3. Implement CSV file parsing - finished.

---
+++ src/test/java/com/reviews/amazon/AmazonReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviews.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
